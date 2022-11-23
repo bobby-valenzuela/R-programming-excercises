5072b2cc0b5dfb8be777f97c4d123b882083fadb
Bobby Valenzuela <70870649+bobby-valenzuela@users.noreply.github.com>
2022-11-23 01:31:41 -0800

    Update ggplot_template.R

--- ggplot_template.R
+++ ggplot_template.R
@@ -1,4 +1,16 @@
 library("ggplot2")
 library("palmerpenguins")
 
+# Single geom
 ggplot(data=penguins)+geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species,shape=species,size=body_mass_g))
+
+# Multi-geom
+ggplot(data=penguins)+
+  geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species,shape=species,size=body_mass_g)) +
+  geom_smooth(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species,shape=species,size=body_mass_g))
+
+# With Annotatioins (Extra labels) Jubstitute/DOOOOOOOOOOOOOOO
+ggplot(data=penguins)+
+  geom_point(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species,shape=species,size=body_mass_g)) +
+  geom_smooth(mapping=aes(x=flipper_length_mm,y=body_mass_g,color=species,shape=species,size=body_mass_g)) + 
+  labs(title="My Chart",subtitle = "My Cool Subtitle", caption = "Data collected by me")
